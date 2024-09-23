ed4c1f459f05f0eb888dc858fc092f120f8b135e
kamala-github04 <vkamala0412@gmail.com>
2024-09-23 21:29:14 +0530

    Delete ex3/db.js

--- ex3/db.js
+++
@@ -1,9 +0,0 @@
-// config/db.js
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/microblogging', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-module.exports = mongoose;
